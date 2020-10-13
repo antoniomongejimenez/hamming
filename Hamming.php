1db40bc070a3d1bb4d0df17111a1ee44ca20e988
Antonio Monge Jiménez <antonio.monge.jimenez@iesdonana.org>
2020-10-13 19:22:32 +0200

    resuelto

--- Hamming.php
+++ Hamming.php
@@ -1,12 +1,27 @@
 <?php
 
 /*
- * Esto es sólo un ESQUELETO para el ejercicio de la "distancia Hamming".
+ * @author Antonio <antonio.monge.jimenez@iesdonana.org>
+ * @copyright Copyright (c) 2020 Antonio
+ * @license GPL3
  */
 
+/**
+ * Calcula la distancia hamming entre dos cadenas de ADN.
+ *
+ * @param string $a La primera cadena.
+ * @param string $b La primera cadena.
+ * @return int|bool El número de caracteres distintos,
+ *                  o false si tienen distinta longitud.
+ */
 function distancia($a, $b)
 {
-    //
-    // TU CÓDIGO VA AQUÍ
-    //
+    if (mb_strlen($a) != mb_strlen($b)) {
+        return false;
+    }
+
+    return count(array_diff_assoc(mb_str_split($a), mb_str_split($b)));
+
 }
+
+
